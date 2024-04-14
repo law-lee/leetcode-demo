443ceaf965703124330ea673fc74d292ca153089
liliangwei.0401 <liliangwei.0401@bytedance.com>
2024-04-14 16:48:45 +0800

    reverse list add recursion

--- reverse_list/reverse_list.go
+++ reverse_list/reverse_list.go
@@ -25,3 +25,37 @@ func help(pre, head *ListNode) *ListNode {
 	head.Next = pre
 	return help(head, next)
 }
+
+// reverse use recursion
+func reverse(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return head
+	}
+	last := reverse(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
+
+// reverseN reverse first nth node
+var successor *ListNode
+
+func reverseN(head *ListNode, n int) *ListNode {
+	if n == 1 {
+		successor = head.Next
+		return head
+	}
+	last := reverseN(head.Next, n-1)
+	head.Next.Next = head
+	head.Next = successor
+	return last
+}
+
+// reverseBetween reverse between m and n
+func reverseBetween(head *ListNode, m, n int) *ListNode {
+	if m == 1 {
+		return reverseN(head, n)
+	}
+	head.Next = reverseBetween(head.Next, m-1, n-1)
+	return head
+}
